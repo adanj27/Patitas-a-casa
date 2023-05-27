236bd0a1f8a591942655f3f44226f99935efae56
Leonel <lpaezbusiness@gmail.com>
2023-05-27 20:22:43 -0300

    Loading Screen en la seccion home

--- frontend/src/vistas/home.jsx
+++ frontend/src/vistas/home.jsx
@@ -76,7 +76,7 @@ const Home = () => {
 	useEffect(() => {
 		setTimeout(() => {
 			setIsLoading(false)
-		}, 2500)
+		}, 3000)
 	}, [])
 	
 
@@ -95,154 +95,149 @@ const Home = () => {
 
 	return (
 		<>
-			{isLoading ? (
+			{isLoading && 
 				<Loading />
-			) : (
-				<div>
-					<header className={styles.header}>
-						<Slider {...settings}>
-							<img className={styles["header__slider--img"]} src={headerImg1} alt="" />
-							<img className={styles["header__slider--img"]} src={headerImg2} alt="" />
-							<img className={styles["header__slider--img"]} src={headerImg3} alt="" />
-						</Slider>
-						<section className={styles.header__section}>
-							<LogoWhite className={styles.header__logo} />
-							<p className={styles.header__title}>"Rescatando patitas, salvando corazones"</p>
-							<div className={styles.header__buttons}>
-								<BtnPrincipal texto="Reportar" setModal={setModal} />
-								<Link to="/adoptar">
-									<BtnPrincipal texto="Adoptar" />
-								</Link>
-							</div>
-						</section>
-					</header>
-
-					<section className={styles['publicacion-destacada']}>
-						<LetraTitulo
-							texto="PUBLICACION RECIENTES"
-							clase={'letra-titulo--red'}
-						/>
-						<h3 className={styles["publicacion-destaca__sub-titulo"]}>
-							Cuando reporta una mascota perdida/encontrada, su publicación
-							aparecerá aquí como publicación reciente.
-						</h3>
-						<TarjetaDestacadaContainer />
-						<div className={styles['publicacion-destacada__buttons']}>
-							<BtnPrincipal texto="Encontrados" />
-							<BtnPrincipal texto="Perdidos" />
-						</div>
-					</section>
-					<div className={styles.adopciones}>
-						<LetraTitulo
-							texto="ADOPCIONES"
-							clase="letra-titulo--red"
-							align="center"
-						/>
-						<AdopcionesTarjetaContainer />
-						<Link to="/adoptar" className={styles.header__buttons}>
+			}
+			<header className={styles.header}>
+				<Slider {...settings}>
+					<img className={styles["header__slider--img"]} src={headerImg1} alt="" />
+					<img className={styles["header__slider--img"]} src={headerImg2} alt="" />
+					<img className={styles["header__slider--img"]} src={headerImg3} alt="" />
+				</Slider>
+				<section className={styles.header__section}>
+					<LogoWhite className={styles.header__logo} />
+					<p className={styles.header__title}>"Rescatando patitas, salvando corazones"</p>
+					<div className={styles.header__buttons}>
+						<BtnPrincipal texto="Reportar" setModal={setModal} />
+						<Link to="/adoptar">
 							<BtnPrincipal texto="Adoptar" />
 						</Link>
 					</div>
-					<LetraTitulo texto="BLOG" clase="letra-titulo--red" align="center" />
-					<BlogContainer />
-					<section className={styles['sobre-nosotros']}>
-						<LetraTitulo texto="Sobre Nosotros" clase="letra-titulo--red" />
-						<article className={styles['sobre-nosotros__container']}>
-							<section className={styles['sobre-nosotros__section']}>
-								<h2>Historia</h2>
-								{listaHistoria.map((lista, id) => {
-									return <Lista texto={lista} key={id} />;
-								})}
-								<p>
-									Nuestra página web surgió de la idea de unir a personas que han
-									perdido a sus mascotas con aquellos que han encontrado a animales
-									en la calle. Al ser amantes de los animales, sabemos lo doloroso
-									que es perder a un amigo peludo y lo gratificante que es poder
-									ayudar a reunir a una mascota con su familia. Por eso, nos
-									propusimos crear esta plataforma para facilitar la búsqueda de
-									mascotas perdidas y ayudar a que puedan regresar a sus hogares.
-								</p>
-								<p>
-									A lo largo del desarrollo de la página, hemos enfrentado desafíos
-									técnicos y logísticos que han requerido soluciones creativas. Pero
-									gracias a la colaboración y el trabajo en equipo, hemos logrado
-									superar cada obstáculo y llevar adelante nuestro proyecto.
-								</p>
-							</section>
-							<section className={styles['sobre-nosotros__section']}>
-								<h2>Misión y visión</h2>
-								{listaMision.map((lista, id) => {
-									return <Lista texto={lista} key={id} />;
-								})}
+				</section>
+			</header>
 
-								<h4>Misión:</h4>
-								<p>
-									En Patitas a Casa nos dedicamos a rescatar y proteger a los
-									animales abandonados o perdidos, y promover la adopción
-									responsable de mascotas. A través de nuestras redes sociales,
-									difundimos información para que los dueños puedan encontrar a sus
-									mascotas y también para fomentar la adopción. Además, trabajamos
-									con refugios para ayudar a que los animales encuentren un hogar y
-									colaboramos con ellos para mejorar las condiciones de vida de los
-									animales en situación de abandono. Nuestro objetivo es lograr un
-									mundo más justo para los animales y concienciar a la sociedad
-									sobre la importancia de su cuidado y protección.
-								</p>
-
-								<h4>Visión:</h4>
-								<p>
-									En Patitas a Casa trabajamos para construir una sociedad más
-									empática y comprometida con la protección de los animales.
-									Queremos ser una organización referente en el rescate y protección
-									de animales, y en la promoción de la adopción responsable. A
-									través de nuestras acciones, buscamos crear conciencia en la
-									sociedad sobre la importancia de cuidar a los animales y
-									contribuir a un cambio cultural que los considere seres sintientes
-									y dignos de respeto. Aspiramos a un futuro donde los animales sean
-									valorados y respetados por toda la sociedad y donde no haya
-									animales en situación de abandono o maltrato.
-								</p>
-							</section>
-						</article>
-						{/* <article className={styles["sobre-nosotros__container"]}>
-							<SobreNosotros 
-								img={imgSobreNosotros}
-								titulo="Lorem"
-								texto="Lorem ipsum dolor sit amet consectetur adipisicing elit.
-								Enim sit qui, repudiandae aperiam laborum a quisquam in fugiat
-								corrupti tenetur reprehenderit, harum, tempora pariatur expedita 
-								excepturi saepe fugit eius nulla."
-							/>
-							<SobreNosotros 
-								img={imgSobreNosotros}
-								titulo="Lorem"
-								texto="Lorem ipsum dolor sit amet consectetur adipisicing elit.
-								Enim sit qui, repudiandae aperiam laborum a quisquam in fugiat
-								corrupti tenetur reprehenderit, harum, tempora pariatur expedita 
-								excepturi saepe fugit eius nulla."
-							/>
-							<SobreNosotros 
-								img={imgSobreNosotros}
-								titulo="Lorem"
-								texto="Lorem ipsum dolor sit amet consectetur adipisicing elit.
-								Enim sit qui, repudiandae aperiam laborum a quisquam in fugiat
-								corrupti tenetur reprehenderit, harum, tempora pariatur expedita 
-								excepturi saepe fugit eius nulla."
-							/>
-							<SobreNosotros 
-								img={imgSobreNosotros}
-								titulo="Lorem"
-								texto="Lorem ipsum dolor sit amet consectetur adipisicing elit.
-								Enim sit qui, repudiandae aperiam laborum a quisquam in fugiat
-								corrupti tenetur reprehenderit, harum, tempora pariatur expedita 
-								excepturi saepe fugit eius nulla."
-							/>
-						</article> */}
-					</section>
-
-					{modal && <Formulario setModal={setModal} />}
+			<section className={styles['publicacion-destacada']}>
+				<LetraTitulo
+					texto="PUBLICACION RECIENTES"
+					clase={'letra-titulo--red'}
+				/>
+				<h3 className={styles["publicacion-destaca__sub-titulo"]}>
+					Cuando reporta una mascota perdida/encontrada, su publicación
+					aparecerá aquí como publicación reciente.
+				</h3>
+					<TarjetaDestacadaContainer />
+					<div className={styles['publicacion-destacada__buttons']}>
+						<BtnPrincipal texto="Encontrados" />
+						<BtnPrincipal texto="Perdidos" />
+					</div>
+				</section>
+				<div className={styles.adopciones}>
+					<LetraTitulo
+						texto="ADOPCIONES"
+						clase="letra-titulo--red"
+						align="center"
+					/>
+					<AdopcionesTarjetaContainer />
+					<Link to="/adoptar" className={styles.header__buttons}>
+						<BtnPrincipal texto="Adoptar" />
+					</Link>
 				</div>
-			)}
+				<LetraTitulo texto="BLOG" clase="letra-titulo--red" align="center" />
+				<BlogContainer />
+				<section className={styles['sobre-nosotros']}>
+					<LetraTitulo texto="Sobre Nosotros" clase="letra-titulo--red" />
+					<article className={styles['sobre-nosotros__container']}>
+						<section className={styles['sobre-nosotros__section']}>
+							<h2>Historia</h2>
+							{listaHistoria.map((lista, id) => {
+								return <Lista texto={lista} key={id} />;
+							})}
+							<p>
+								Nuestra página web surgió de la idea de unir a personas que han
+								perdido a sus mascotas con aquellos que han encontrado a animales
+								en la calle. Al ser amantes de los animales, sabemos lo doloroso
+								que es perder a un amigo peludo y lo gratificante que es poder
+								ayudar a reunir a una mascota con su familia. Por eso, nos
+								propusimos crear esta plataforma para facilitar la búsqueda de
+								mascotas perdidas y ayudar a que puedan regresar a sus hogares.
+							</p>
+							<p>
+								A lo largo del desarrollo de la página, hemos enfrentado desafíos
+								técnicos y logísticos que han requerido soluciones creativas. Pero
+								gracias a la colaboración y el trabajo en equipo, hemos logrado
+								superar cada obstáculo y llevar adelante nuestro proyecto.
+							</p>
+						</section>
+						<section className={styles['sobre-nosotros__section']}>
+							<h2>Misión y visión</h2>
+							{listaMision.map((lista, id) => {
+								return <Lista texto={lista} key={id} />;
+							})}
+							<h4>Misión:</h4>
+							<p>
+								En Patitas a Casa nos dedicamos a rescatar y proteger a los
+								animales abandonados o perdidos, y promover la adopción
+								responsable de mascotas. A través de nuestras redes sociales,
+								difundimos información para que los dueños puedan encontrar a sus
+								mascotas y también para fomentar la adopción. Además, trabajamos
+								con refugios para ayudar a que los animales encuentren un hogar y
+								colaboramos con ellos para mejorar las condiciones de vida de los
+								animales en situación de abandono. Nuestro objetivo es lograr un
+								mundo más justo para los animales y concienciar a la sociedad
+								sobre la importancia de su cuidado y protección.
+							</p>
+							<h4>Visión:</h4>
+							<p>
+								En Patitas a Casa trabajamos para construir una sociedad más
+								empática y comprometida con la protección de los animales.
+								Queremos ser una organización referente en el rescate y protección
+								de animales, y en la promoción de la adopción responsable. A
+								través de nuestras acciones, buscamos crear conciencia en la
+								sociedad sobre la importancia de cuidar a los animales y
+								contribuir a un cambio cultural que los considere seres sintientes
+								y dignos de respeto. Aspiramos a un futuro donde los animales sean
+								valorados y respetados por toda la sociedad y donde no haya
+								animales en situación de abandono o maltrato.
+							</p>
+						</section>
+					</article>
+					{/* <article className={styles["sobre-nosotros__container"]}>
+						<SobreNosotros 
+							img={imgSobreNosotros}
+							titulo="Lorem"
+							texto="Lorem ipsum dolor sit amet consectetur adipisicing elit.
+							Enim sit qui, repudiandae aperiam laborum a quisquam in fugiat
+							corrupti tenetur reprehenderit, harum, tempora pariatur expedita 
+							excepturi saepe fugit eius nulla."
+						/>
+						<SobreNosotros 
+							img={imgSobreNosotros}
+							titulo="Lorem"
+							texto="Lorem ipsum dolor sit amet consectetur adipisicing elit.
+							Enim sit qui, repudiandae aperiam laborum a quisquam in fugiat
+							corrupti tenetur reprehenderit, harum, tempora pariatur expedita 
+							excepturi saepe fugit eius nulla."
+						/>
+						<SobreNosotros 
+							img={imgSobreNosotros}
+							titulo="Lorem"
+							texto="Lorem ipsum dolor sit amet consectetur adipisicing elit.
+							Enim sit qui, repudiandae aperiam laborum a quisquam in fugiat
+							corrupti tenetur reprehenderit, harum, tempora pariatur expedita 
+							excepturi saepe fugit eius nulla."
+						/>
+						<SobreNosotros 
+							img={imgSobreNosotros}
+							titulo="Lorem"
+							texto="Lorem ipsum dolor sit amet consectetur adipisicing elit.
+							Enim sit qui, repudiandae aperiam laborum a quisquam in fugiat
+							corrupti tenetur reprehenderit, harum, tempora pariatur expedita 
+							excepturi saepe fugit eius nulla."
+						/>
+					</article> */}
+				</section>
+
+				{modal && <Formulario setModal={setModal} />}
 		</>
 	);
 };
