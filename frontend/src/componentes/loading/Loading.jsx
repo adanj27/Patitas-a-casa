76edbb767ec687e3d24f1af4e3470b1987bc321d
Leonel <lpaezbusiness@gmail.com>
2023-05-29 19:29:09 -0300

    Logo blanco en pantalla de carga

--- frontend/src/componentes/loading/Loading.jsx
+++ frontend/src/componentes/loading/Loading.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { LogoWhite } from "../../iconos/svg/icons_svg";
+import LogoWhite from "../../iconos/logo-patitas-blanco.png";
 import styles from "../../css/home/loading.module.css"
 
 const Loading = () => {
   return (
     <>
       <div className={styles.loading__container}>
-        <LogoWhite className={styles.loading__img} />
+        <img src={LogoWhite} className={styles.loading__img} alt="logo patitas blanco" />
       </div>
     </>
   )
