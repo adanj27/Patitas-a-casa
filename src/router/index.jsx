3193f8fd9f21d6bc1d4d073ea550d610203504fc
Speedev <roberto.tg99@gmail.com>
2023-05-29 19:15:24 -0500

    :bug: Fix route name

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -35,7 +35,7 @@ export const router = createBrowserRouter([
         element: <Contacto />
       },
       {
-        path: "pedidos",
+        path: "perdidos",
         element: <Perdidos />
       },
       {
