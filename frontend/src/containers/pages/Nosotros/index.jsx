29bb1e9000945c69d2bd2daf084024781c4caf8f
Leonel <lpaezbusiness@gmail.com>
2023-07-24 14:31:36 -0300

    Información agrega a pagina nosotros + cambios estilo

--- frontend/src/containers/pages/Nosotros/index.jsx
+++ frontend/src/containers/pages/Nosotros/index.jsx
@@ -1,6 +1,8 @@
 // Css
 import styles from './styles.module.css';
 
+import { devsData } from "../../../data/dev-data";
+
 // Imagenes
 import PerroJugando from "/icons/perro-jugando.png"
 
@@ -10,17 +12,29 @@ export const Nosotros = () => {
       <div className={styles.nosotros__headings}>
         <h2 className={styles.nosotros__title}>Sobre Nosotros</h2>
         <p className={styles.nosotros__subtitle}>
-        Somos un equipo que nos une el amor por los animales y que sufrimos situaciones por las que vos quizás estas pasando ahora mismo.
+         Somos un equipo que nos une el amor por los animales y que sufrimos situaciones por las que vos quizás estas pasando ahora mismo.
         </p>
       </div>
       <div className={styles["nosotros-info__container"]}>
 
         <div className={`${styles.nosotros__item} ${styles["nosotros__item--large"]}`}>
           <h3 className={styles["nosotros__item-title"]}>¡Conócenos!</h3>
+          <div className={styles.nosotros__devs}>
+            {devsData.map((dev, index) => {
+              return (
+                <img
+                  className={styles.nosotros__dev}
+                  src={dev.img}
+                  alt={`foto ${dev.title} desarrollador`}
+                  key={index} 
+                />
+              )
+            })}
+          </div>
           <p className={styles["nosotros__item-text"]}>
-          Somos el equipo de Patitas a Casas un grupo de desarrolladores que nos encargamos de diseñar y idear toda esta herramienta.
-          <br />
-          Nuestro equipo se renueva constantemente con nuevas personas y habilidades distintas, si quieres formar parte de nosotros ponte en contacto con nosotros a través de un correo electrónico y tendremos una charla para que trabajemos juntos.
+            Somos el equipo de Patitas a Casas un grupo de desarrolladores que nos encargamos de diseñar y idear toda esta herramienta.
+            <br />
+            Nuestro equipo se renueva constantemente con nuevas personas y habilidades distintas, si quieres formar parte de nosotros ponte en contacto con nosotros a través de un correo electrónico y tendremos una charla para que trabajemos juntos.
           </p>
         </div>
 
@@ -28,13 +42,13 @@ export const Nosotros = () => {
           <div className={styles.nosotros__item}>
             <h3 className={styles["nosotros__item-title"]}>Lo que intentamos con esto:</h3>
             <p className={styles["nosotros__item-text"]}>
-            Lo que intentamos con este proyecto es ayudar a todas esas personas que perdieron una mascota, de forma totalmente gratuita. También apuntamos a sumar nuevos integrantes a las familias con nuestro apartado exclusivo para animalitos en adopción.
+              Lo que intentamos con este proyecto es ayudar a todas esas personas que perdieron a una mascota, de forma totalmente gratuita. También apuntamos a sumar nuevos integrantes a las familias con nuestro a apartado exclusivo para animalitos en adopción 
             </p>
           </div>
           <div className={styles.nosotros__item}>
             <h3 className={styles["nosotros__item-title"]}>Como lo hicimos</h3>
             <p className={styles["nosotros__item-text"]}>
-            Todo empezó siendo diseñado por una persona, posteriormente se publicó un comentario en un canal de desarrolladores y desde ahí, solo el equipo se expandió. Todos estos son desarrolladores y diseñadores jóvenes, apasionados por su trabajo y se unieron a este proyecto para ampliar sus conocimientos y experiencia laboral.
+              Todo empezó siendo diseñado por una persona, posteriormente se publico un comentario en un canal de desarrolladores y desde ahí el equipo se expandió. Todos estos son desarrolladores y diseñadores jóvenes apasionados por su trabajo y se unieron a este proyecto para ampliar sus conocimientos y experiencia laboral. 
             </p>
           </div>
         </div>
@@ -47,7 +61,8 @@ export const Nosotros = () => {
               <div className={styles.nosotros__item}>
                 <h3 className={styles["nosotros__item-title"]}>Porque lo hicimos:</h3>
                 <p className={styles["nosotros__item-text"]}>
-                Para contribuir al bienestar de las mascotas que carecen de un hogar es una acción admirable y significativa. Al brindarles ayuda y la posibilidad de regresar a sus familias, se promueve un ambiente de amor y cuidado para estos seres queridos de cuatro patas. Además, esta noble causa no solo impacta positivamente en la vida de las mascotas, sino también en la comunidad en general, creando conciencia sobre la importancia de la adopción responsable y el cuidado responsable de los animales domésticos. Asimismo, se fomenta el desarrollo de programas y recursos que permitan incrementar la efectividad de esta labor y alcanzar a un mayor número de animales necesitados. Cada esfuerzo cuenta y, al colaborar juntos en esta misión, podemos lograr un cambio positivo en la vida de aquellos que más nos necesitan.                </p>
+                  Para contribuir al bienestar de las mascotas que carecen de un hogar es una acción admirable y significativa. Al brindarles ayuda y la posibilidad de regresar a sus familias, se promueve un ambiente de amor y cuidado para estos seres queridos de cuatro patas. Además, esta noble causa no solo impacta positivamente en la vida de las mascotas, sino también en la comunidad en general, creando conciencia sobre la importancia de la adopción responsable y el cuidado responsable de los animales domésticos.        
+                </p>
               </div>
             </div>
           </div>
@@ -60,24 +75,19 @@ export const Nosotros = () => {
           <div className={styles.nosotros__item}>
             <h3 className={styles["nosotros__item-title"]}>Inicio</h3>
             <p className={styles["nosotros__item-text"]}>
-            Todo empezó con una idea. Esta idea se fue comentando a distintas personas para ir mejorándola y buscando si era algo que tenía utilidad. Una vez que nos dimos cuenta de que era una buena idea y tenía un futuro, empezamos con la planificación.
+             Todo inicio con una idea, esta idea se fue comentando a distintas personas para ir mejorándola y buscando si era algo que tenia utilidad, y una vez que nos dimos cuenta que era una buena idea y tenia un futuro, empezamos con la planeación.
             </p>
           </div>
           <div className={styles.nosotros__item}>
             <h3 className={styles["nosotros__item-title"]}>Planear</h3>
             <p className={styles["nosotros__item-text"]}>
-            Se inició por darle una forma a la idea e ir compartiéndola por diferentes canales de comunicación para poder buscar gente que quiera colaborar con el proyecto. Una vez que se empezó a diseñar las páginas y las funcionalidades bien definidas, se dio inicio a la fase de desarrollo y mejora continua.            </p>
-          </div>
-          <div className={styles.nosotros__item}>
-            <h3 className={styles["nosotros__item-title"]}>Desarrollo</h3>
-            <p className={styles["nosotros__item-text"]}>
-            En enero de 2022, se inició el desarrollo y constante mejora de "Patitas a Casa". En cada paso de la construcción de los módulos del sitio web y cada una de sus funcionalidades, buscamos que sea lo mejor y más útil posible para cada una de las personas que la utilicen.
+              Se inicio por darle una forma a la idea y ir compartiéndola por diferentes canales de comunicación para poder buscar gente que quiera ir colaborando con el proyecto, una vez que se empezó a diseñar la paginas y las funcionalidades bien definidas se empezó la fase de desarrollo y mejora continua. 
             </p>
           </div>
           <div className={styles.nosotros__item}>
             <h3 className={styles["nosotros__item-title"]}>El proyecto hoy en día</h3>
             <p className={styles["nosotros__item-text"]}>
-            Hasta el día de hoy, esta herramienta sigue en desarrollo, y contamos con un equipo que aprende todos los días de este proyecto, listo para solucionar todos los inconvenientes que se nos presenten en el camino, para que esta herramienta siga cumpliendo su propósito y ayude a toda la gente posible.
+              En enero del 2022 se empezó el desarrollo y constante mejora de “Patitas a Casa” en cada paso de la construcción de los módulos del sitio web y cada una de sus funcionalidades para que sea lo mejor y mas útil posible para cada unas de las personas que la utilicen.
             </p>
           </div>
         </div>
@@ -85,13 +95,15 @@ export const Nosotros = () => {
         <div className={`${styles.nosotros__item} ${styles["nosotros__item--large"]} ${styles["text-left"]}`}>
           <h3 className={styles["nosotros__item-title"]}>Lo que es hoy en día</h3>
           <p className={`${styles["nosotros__item-text"]}`}>
-          Hoy “Patitas a Casa” es una herramienta por y para personas que perdieron a una mascota alguna vez en su vida, todo esto de forma totalmente gratuita, y esta herramienta funciona gracias a todos los que la usen tanto para reportar una perdida o alguna mascota que parece estar perdida, como principal método de difusión de las tarjetas de reportes usamos las redes sociales, por eso es muy importante que esta herramienta la usemos todos correctamente y si tienes alguna duda consulta al soporte técnico o por medio de nuestras redes sociales.           </p>
+            Hoy “Patitas a Casa” es una herramienta por y para personas que perdieron a una mascota alguna vez en su vida, todo esto de forma totalmente gratuita, y esta herramienta funciona gracias a todos los que la usen tanto para reportar una perdida o alguna mascota que parece estar perdida, como principal método de difusión de las tarjetas de reportes usamos las redes sociales, por eso es muy importante que esta herramienta la usemos todos correctamente y si tienes alguna duda consulta al soporte técnico o por medio de nuestras redes sociales.
+          </p>
         </div>
         
         <div className={`${styles.nosotros__item} ${styles["nosotros__item--large"]} ${styles["text-left"]}`}>
           <h3 className={styles["nosotros__item-title"]}>A lo que apuntamos</h3>
           <p className={styles["nosotros__item-text"]}>
-          El objetivo principal de esta herramienta es garantizar que se vuelva cada vez más útil para todos los usuarios, logrando aumentar significativamente la cantidad de mascotas encontradas y fomentando su uso de manera constante. Además, se prevé incorporar una amplia gama de nuevas funcionalidades que contribuyan a potenciar su eficacia y comodidad en el proceso de búsqueda y localización de nuestras queridas mascotas perdidas o extraviadas.          </p>
+            El objetivo principal de esta herramienta es garantizar que se vuelva cada vez más útil para todos los usuarios, logrando aumentar significativamente la cantidad de mascotas encontradas y fomentando su uso de manera constante. Además, se prevé incorporar una amplia gama de nuevas funcionalidades que contribuyan a potenciar su eficacia y comodidad en el proceso de búsqueda y localización de nuestras queridas mascotas perdidas o extraviadas.
+          </p>
         </div>
       </div>
     </div>
---
+++ frontend/src/data/dev-data.js
@@ -0,0 +1,42 @@
+export const devsData = [
+  {
+    title: "nombre",
+    img: "https://i.pinimg.com/280x280_RS/66/a1/a4/66a1a4a22e271ec3ee3ef35ba0883ca4.jpg"
+  },
+  {
+    title: "nombre",
+    img: "https://i.pinimg.com/280x280_RS/66/a1/a4/66a1a4a22e271ec3ee3ef35ba0883ca4.jpg"
+  },
+  {
+    title: "nombre",
+    img: "https://i.pinimg.com/280x280_RS/66/a1/a4/66a1a4a22e271ec3ee3ef35ba0883ca4.jpg"
+  },
+  {
+    title: "nombre",
+    img: "https://i.pinimg.com/280x280_RS/66/a1/a4/66a1a4a22e271ec3ee3ef35ba0883ca4.jpg"
+  },
+  {
+    title: "nombre",
+    img: "https://i.pinimg.com/280x280_RS/66/a1/a4/66a1a4a22e271ec3ee3ef35ba0883ca4.jpg"
+  },
+  {
+    title: "nombre",
+    img: "https://i.pinimg.com/280x280_RS/66/a1/a4/66a1a4a22e271ec3ee3ef35ba0883ca4.jpg"
+  },
+  {
+    title: "nombre",
+    img: "https://i.pinimg.com/280x280_RS/66/a1/a4/66a1a4a22e271ec3ee3ef35ba0883ca4.jpg"
+  },
+  {
+    title: "nombre",
+    img: "https://i.pinimg.com/280x280_RS/66/a1/a4/66a1a4a22e271ec3ee3ef35ba0883ca4.jpg"
+  },
+  {
+    title: "nombre",
+    img: "https://i.pinimg.com/280x280_RS/66/a1/a4/66a1a4a22e271ec3ee3ef35ba0883ca4.jpg"
+  },
+  {
+    title: "nombre",
+    img: "https://i.pinimg.com/280x280_RS/66/a1/a4/66a1a4a22e271ec3ee3ef35ba0883ca4.jpg"
+  },
+]
