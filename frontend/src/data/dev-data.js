b283c59102bf2fc0800a5a99d23efe8a0c9f2f93
Leonel <lpaezbusiness@gmail.com>
2023-08-06 20:19:10 -0300

    Fotos de devs añadidas

--- frontend/src/data/dev-data.js
+++ frontend/src/data/dev-data.js
@@ -1,19 +1,19 @@
 export const devsData = [
   {
-    title: "nombre",
-    img: "https://i.pinimg.com/280x280_RS/66/a1/a4/66a1a4a22e271ec3ee3ef35ba0883ca4.jpg"
+    title: "zeroz",
+    img: "/icons/devs/zeroz.png"
   },
   {
-    title: "nombre",
-    img: "https://i.pinimg.com/280x280_RS/66/a1/a4/66a1a4a22e271ec3ee3ef35ba0883ca4.jpg"
+    title: "engrohm",
+    img: "/icons/devs/engrohm.jpg"
   },
   {
-    title: "nombre",
-    img: "https://i.pinimg.com/280x280_RS/66/a1/a4/66a1a4a22e271ec3ee3ef35ba0883ca4.jpg"
+    title: "heis",
+    img: "/icons/devs/heis.webp"
   },
   {
-    title: "nombre",
-    img: "https://i.pinimg.com/280x280_RS/66/a1/a4/66a1a4a22e271ec3ee3ef35ba0883ca4.jpg"
+    title: "leo",
+    img: "/icons/devs/leo.jpg"
   },
   {
     title: "nombre",
