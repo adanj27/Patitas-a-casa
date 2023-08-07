1695048d7776ba5fc7b8bfdff9f0c45014008464
Leonel <lpaezbusiness@gmail.com>
2023-08-07 16:25:38 -0300

    Popup info sobre desarrolladores en nosotros

--- frontend/src/data/dev-data.js
+++ frontend/src/data/dev-data.js
@@ -1,42 +1,82 @@
 export const devsData = [
   {
     title: "engrohm",
-    img: "/icons/devs/engrohm.jpg"
+    name: "mauricio",
+    img: "/icons/devs/engrohm.jpg",
+    position: "ceo",
+    linkedin: "",
+    portfolio: ""
   },
   {
     title: "zeroz",
-    img: "/icons/devs/zeroz.png"
+    name: "",
+    img: "/icons/devs/zeroz.png",
+    position: "desarrollador",
+    linkedin: "",
+    portfolio: ""
   },
   {
     title: "heis",
-    img: "/icons/devs/heis.webp"
+    name: "",
+    img: "/icons/devs/heis.webp",
+    position: "desarrollador",
+    linkedin: "",
+    portfolio: ""
   },
   {
     title: "leo",
-    img: "/icons/devs/leo.jpg"
+    name: "leonel",
+    img: "/icons/devs/leo.jpg",
+    position: "desarrollador",
+    linkedin: "",
+    portfolio: ""
   },
   {
     title: "letal",
-    img: "/icons/devs/letal.png"
+    name: "",
+    img: "/icons/devs/letal.png",
+    position: "desarrollador",
+    linkedin: "",
+    portfolio: ""
+  },
+  {
+    title: "leire",
+    name: "",
+    img: "https://i.pinimg.com/280x280_RS/66/a1/a4/66a1a4a22e271ec3ee3ef35ba0883ca4.jpg",
+    position: "diseñadora",
+    linkedin: "",
+    portfolio: ""
+  },
+  {
+    title: "augus",
+    name: "",
+    img: "https://i.pinimg.com/280x280_RS/66/a1/a4/66a1a4a22e271ec3ee3ef35ba0883ca4.jpg",
+    position: "desarrollador",
+    linkedin: "",
+    portfolio: ""
   },
   {
     title: "nombre",
-    img: "https://i.pinimg.com/280x280_RS/66/a1/a4/66a1a4a22e271ec3ee3ef35ba0883ca4.jpg"
+    name: "",
+    img: "https://i.pinimg.com/280x280_RS/66/a1/a4/66a1a4a22e271ec3ee3ef35ba0883ca4.jpg",
+    position: "",
+    linkedin: "",
+    portfolio: ""
   },
   {
     title: "nombre",
-    img: "https://i.pinimg.com/280x280_RS/66/a1/a4/66a1a4a22e271ec3ee3ef35ba0883ca4.jpg"
+    name: "",
+    img: "https://i.pinimg.com/280x280_RS/66/a1/a4/66a1a4a22e271ec3ee3ef35ba0883ca4.jpg",
+    position: "",
+    linkedin: "",
+    portfolio: ""
   },
   {
     title: "nombre",
-    img: "https://i.pinimg.com/280x280_RS/66/a1/a4/66a1a4a22e271ec3ee3ef35ba0883ca4.jpg"
-  },
-  {
-    title: "nombre",
-    img: "https://i.pinimg.com/280x280_RS/66/a1/a4/66a1a4a22e271ec3ee3ef35ba0883ca4.jpg"
-  },
-  {
-    title: "nombre",
-    img: "https://i.pinimg.com/280x280_RS/66/a1/a4/66a1a4a22e271ec3ee3ef35ba0883ca4.jpg"
+    name: "",
+    img: "https://i.pinimg.com/280x280_RS/66/a1/a4/66a1a4a22e271ec3ee3ef35ba0883ca4.jpg",
+    position: "",
+    linkedin: "",
+    portfolio: ""
   },
 ]
