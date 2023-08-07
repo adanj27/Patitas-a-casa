c2af44054db30a1f73856ac7d223d75f487ff583
Leonel <lpaezbusiness@gmail.com>
2023-08-06 21:55:09 -0300

    Foto dev añadida

--- frontend/src/data/dev-data.js
+++ frontend/src/data/dev-data.js
@@ -16,8 +16,8 @@ export const devsData = [
     img: "/icons/devs/leo.jpg"
   },
   {
-    title: "nombre",
-    img: "https://i.pinimg.com/280x280_RS/66/a1/a4/66a1a4a22e271ec3ee3ef35ba0883ca4.jpg"
+    title: "letal",
+    img: "/icons/devs/letal.png"
   },
   {
     title: "nombre",
