d88d8731aa33e2e8e484e55f42507b708ec79d9d
Leonel <lpaezbusiness@gmail.com>
2023-08-06 21:53:46 -0300

    Fix padding nav

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,9 +7,7 @@ import { Suspense } from "react"
 import Spinner from "./components/Spinner/index"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <div className="App">
-    <Suspense fallback={<Spinner />}>
-      <RouterProvider router={router} />
-    </Suspense>
-  </div>
+  <Suspense fallback={<Spinner />}>
+    <RouterProvider router={router} />
+  </Suspense>
 )
