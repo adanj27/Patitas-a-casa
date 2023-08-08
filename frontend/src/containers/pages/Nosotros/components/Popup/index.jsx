b1407382300e2ee91055277315c281e9f393c660
Leonel <lpaezbusiness@gmail.com>
2023-08-08 17:23:12 -0300

    Nombre y cargo añadido a popup devs

--- frontend/src/containers/pages/Nosotros/components/Popup/index.jsx
+++ frontend/src/containers/pages/Nosotros/components/Popup/index.jsx
@@ -10,8 +10,8 @@ const Popup = ({ isOpen, onClose, dev }) => {
       <div className={styles.popupContent}>
         <span className={styles.closeBtn} onClick={onClose}>&times;</span>
         <img className={styles.popup__dev} src={dev.img} alt={`foto ${dev.title} desarrollador`} />
-        <p className={styles.popup__name}>{dev.name}</p>
-        <p className={styles.popup__position}>{dev.position}</p>
+        <p className={styles.popup__name}>{dev.name || "Nombre"}</p>
+        <p className={styles.popup__position}>{dev.position || "Position"}</p>
         <a className={styles.popup__link} href={dev.linkedin}>LinkedIn</a>
         <a className={styles.popup__link} href={dev.portfolio}>Portafolio</a>
         <img className={styles.popup__logo} src={LogoPatitas} alt="logo patitas a casa" />
