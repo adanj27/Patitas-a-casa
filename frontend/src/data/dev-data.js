c469af1990cc703e9f5fe7a585de0f936d7c456d
NegroHm <hh.mauri2190@gmail.com>
2023-08-09 23:40:22 -0300

    imgs dev

--- frontend/src/data/dev-data.js
+++ frontend/src/data/dev-data.js
@@ -4,60 +4,60 @@ export const devsData = [
     name: "mauricio",
     img: "/icons/devs/engrohm.jpg",
     position: "ceo",
-    linkedin: "",
-    portfolio: ""
+    linkedin: "https://www.linkedin.com/in/mauricio-hector-medina-4b9048234",
+    // portfolio: ""
   },
   {
     title: "zeroz",
-    name: "",
+    name: "Adan",
     img: "/icons/devs/zeroz.png",
     position: "desarrollador",
-    linkedin: "",
-    portfolio: ""
+    linkedin: "https://www.linkedin.com/in/adan-jimenez-dev/",
+    portfolio: "https://adan-jimenez.vercel.app/"
   },
   {
     title: "heis",
-    name: "",
+    name: "Juan David",
     img: "/icons/devs/heis.webp",
     position: "desarrollador",
-    linkedin: "",
-    portfolio: ""
+    linkedin: "https://www.linkedin.com/in/juan-david-moreno-883a46233",
+    portfolio: "https://heisjuanda.netlify.app/"
   },
   {
     title: "leo",
     name: "leonel",
     img: "/icons/devs/leo.jpg",
     position: "desarrollador",
-    linkedin: "",
-    portfolio: ""
+    linkedin: "https://www.linkedin.com/in/leonel-paez/",
+    portfolio: "https://lp-portfolio.vercel.app/"
   },
   {
     title: "letal",
-    name: "",
+    name: "Letal",
     img: "/icons/devs/letal.png",
     position: "desarrollador",
-    linkedin: "",
-    portfolio: ""
+    linkedin: "https://www.linkedin.com/in/letalandroid/",
+    portfolio: "https://letalandroid.online/#redes  "
   },
   {
     title: "leire",
-    name: "",
-    img: "https://i.pinimg.com/280x280_RS/66/a1/a4/66a1a4a22e271ec3ee3ef35ba0883ca4.jpg",
+    name: "Leire",
+    img: "/icons/devs/leire.jpg",
     position: "diseñadora",
-    linkedin: "",
-    portfolio: ""
+    // linkedin: "",
+    // portfolio: ""
   },
   {
     title: "augus",
-    name: "",
-    img: "https://i.pinimg.com/280x280_RS/66/a1/a4/66a1a4a22e271ec3ee3ef35ba0883ca4.jpg",
+    name: "Augus",
+    img: "/icons/devs/augus.jpg",
     position: "desarrollador",
-    linkedin: "",
-    portfolio: ""
+    linkedin: "https://www.linkedin.com/in/augusto-borromeo-b75255218",
+    portfolio: "https://www.augusdev.com/portfolio"
   },
   {
     title: "nombre",
-    name: "",
+    name: "Santiago",
     img: "https://i.pinimg.com/280x280_RS/66/a1/a4/66a1a4a22e271ec3ee3ef35ba0883ca4.jpg",
     position: "",
     linkedin: "",
@@ -65,11 +65,11 @@ export const devsData = [
   },
   {
     title: "nombre",
-    name: "",
-    img: "https://i.pinimg.com/280x280_RS/66/a1/a4/66a1a4a22e271ec3ee3ef35ba0883ca4.jpg",
-    position: "",
+    name: "Manzana",
+    img: "/icons/devs/manzana.jpg",
+    position: "Desarrollador",
     linkedin: "",
-    portfolio: ""
+    portfolio: "https://github.com/levioneyh"
   },
   {
     title: "nombre",
