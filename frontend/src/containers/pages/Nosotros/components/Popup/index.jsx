6f6c86fd06058292ca46203467d1235f703bc6aa
Juan David Moreno Alfonso <jdma253@gmail.com>
2023-08-13 11:10:21 -0500

    handle close when user click outsite the popUps

--- frontend/src/containers/pages/Nosotros/components/Popup/index.jsx
+++ frontend/src/containers/pages/Nosotros/components/Popup/index.jsx
@@ -5,9 +5,13 @@ import LogoPatitas from "/icons/logo-patita.png"
 const Popup = ({ isOpen, onClose, dev }) => {
   if (!isOpen) return null;
 
+  const stopCloseEventPropagation =  (e) => {
+    e.stopPropagation();
+  };
+
   return (
-    <div className={styles.popup}>
-      <div className={styles.popupContent}>
+    <div className={styles.popup} onClick={onClose}>
+      <div className={styles.popupContent} onClick={stopCloseEventPropagation}>
         <span className={styles.closeBtn} onClick={onClose}>&times;</span>
         <img className={styles.popup__dev} src={dev.img} alt={`foto ${dev.title} desarrollador`} />
         <p className={styles.popup__name}>{dev.name || "Nombre"}</p>
