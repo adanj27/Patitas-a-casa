e9070141571d48997b83ad88db47152606caa44d
A. J <adanj270594@gmail.com>
2023-11-27 18:29:49 -0400

    feat: :sparkles: feat: add endpoint pets

--- frontend/src/hooks/services/usePost.js
+++ frontend/src/hooks/services/usePost.js
@@ -1,13 +1,28 @@
 import { useState } from "react";
 import axios from 'axios'
+import { useAuth } from '../../context/AuthContext';
 
 //api/url
 const usePost = (url, data) => {
-    const [ status, setStatus ] = useState();
+    const [status, setStatus] = useState();
+	const { isAuthenticated } = useAuth(); 
+    
 
     const post = async () => {
         try {
-            await axios.post(url, data)
+            if (!isAuthenticated) {
+                console.error('Usuario no autenticado');
+                setStatus(403);
+                return;
+            }
+
+            const token = localStorage.getItem('token');
+            await axios.post(url, data, {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+
             setStatus(200)
         } catch (error) {
             setStatus(403)
