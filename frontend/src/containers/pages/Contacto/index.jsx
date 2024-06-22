dd6e73a32c2037e9955745b927632b95aae20e7c
A. J <adanj270594@gmail.com>
2024-06-22 09:02:00 -0400

    update: contact form

--- frontend/src/containers/pages/Contacto/index.jsx
+++ frontend/src/containers/pages/Contacto/index.jsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-import { BtnPrincipal } from "../../../components";
-import { Notification } from "../../../components/Notification/Index";
-import styles from "./styles.module.css";
+import { BtnPrincipal } from '../../../components';
+import { Notification } from '../../../components/Notification/Index';
+import styles from './styles.module.css';
 
 // import emailjs from '@emailjs/browser';
 
@@ -14,18 +14,18 @@ const Contacto = () => {
   const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   const phonePattern = /^\d{10}$/;
 
-  const [firstName, setFirstName] = useState(""),
-    [lastName, setLastName] = useState(""),
-    [email, setEmail] = useState(""),
-    [phone, setPhone] = useState(""),
-    [message, setMessage] = useState(""),
+  const [firstName, setFirstName] = useState(''),
+    [lastName, setLastName] = useState(''),
+    [email, setEmail] = useState(''),
+    [phone, setPhone] = useState(''),
+    [message, setMessage] = useState(''),
     [showNotification, setShowNotification] = useState(false),
     [notification, setNotification] = useState('');
 
   const handleChangeInput = (event, setFunction) => {
     event.target.value.length > 0
-      ? (event.target.style.borderBottom = "1px solid #ff0004b2")
-      : (event.target.style.borderBottom = "1px solid #575756");
+      ? (event.target.style.borderBottom = '1px solid #ff0004b2')
+      : (event.target.style.borderBottom = '1px solid #575756');
     setFunction(event.target.value);
   };
 
@@ -33,20 +33,20 @@ const Contacto = () => {
     event.preventDefault();
 
     let check = true;
-    let failedProp = "";
-    let inputError = "";
+    let failedProp = '';
+    let inputError = '';
 
     const maxMinLength = (field, min, max) => {
       if (field.length < min)
         return [false, `no tiene la longitud mínima de ${min}`];
       if (field.length > max)
         return [false, `es demasiado largo, máximo ${max}} carácteres`];
-      return [true, ""];
+      return [true, ''];
     };
 
     const matchPattern = (field, pattern) => {
       if (!field.match(pattern)) return [false, `no es válido`];
-      return [true, ""];
+      return [true, ''];
     };
 
     const refactor = {
@@ -67,40 +67,39 @@ const Contacto = () => {
     }
     setShowNotification(true);
     if (check) {
-
       const emailParams = {
-        to_name: "Patitas A Casa", // Puedes personalizar el destinatario
+        to_name: 'Patitas A Casa', // Puedes personalizar el destinatario
         from_name: `${firstName} ${lastName}`,
         from_email: email,
         phone_number: phone,
         message: message,
       };
-      
-      emailjs
-      .send(
-        EMAILJS_SERVICE_ID,
-        EMAILJS_TEMPLATE_ID,
-        emailParams,
-        EMAILJS_USER_ID
-      )
-      .then(
-        (response) => {
-          console.log("Correo enviado con éxito", response);
-          setShowNotification(true);
-          setNotification("Correo enviado con éxito");
 
-          setFirstName("");
-        setLastName("");
-        setEmail("");
-        setPhone("");
-        setMessage("");
-        },
-        (error) => {
-          console.error("Error al enviar el correo", error);
-          setShowNotification(true);
-          setNotification("Error al enviar el correo");
-        }
-      );
+      emailjs
+        .send(
+          EMAILJS_SERVICE_ID,
+          EMAILJS_TEMPLATE_ID,
+          emailParams,
+          EMAILJS_USER_ID
+        )
+        .then(
+          (response) => {
+            console.log('Correo enviado con éxito', response);
+            setShowNotification(true);
+            setNotification('Correo enviado con éxito');
+
+            setFirstName('');
+            setLastName('');
+            setEmail('');
+            setPhone('');
+            setMessage('');
+          },
+          (error) => {
+            console.error('Error al enviar el correo', error);
+            setShowNotification(true);
+            setNotification('Error al enviar el correo');
+          }
+        );
     } else {
       setShowNotification(true);
       setNotification(`El campo ${failedProp} ${inputError}`);
@@ -108,45 +107,50 @@ const Contacto = () => {
   };
 
   const textareaStyle = {
-    height: message === "" ? "auto" : "3.5rem", // Cambia la altura según el contenido
+    height: message === '' ? 'auto' : '3.5rem', // Cambia la altura según el contenido
   };
 
   return (
     <>
-      {showNotification && <Notification setShowModal={setShowNotification} message={notification} />}
-      <div className={styles["contenedor-principal"]}>
+      {showNotification && (
+        <Notification
+          setShowModal={setShowNotification}
+          message={notification}
+        />
+      )}
+      <div className={styles['contenedor-principal']}>
         <form onSubmit={handleSubmit} action="/" className={styles.contacto}>
-          <div className={styles["contacto__information"]}>
+          <div className={styles['contacto__information']}>
             <header>
               <h2>Contacta con el equipo de “Patitas a Casa”</h2>
               <h5>
                 Una gran visión sin grandes personas es irrelevante. Trabajemos
-                Juntos.
+                juntos.
               </h5>
             </header>
             <article>
               <p>
-                Si deseas aportar con tu idea o trabajar con nosotros y sentís
-                que tienes alguna habilidad para aportar al equipo llena este
-                formulario y te contestaremos a la brevedad.
+                Si deseas colaborar con nosotros y consideras que tienes
+                habilidades que podrían aportar al equipo, por favor completa
+                este formulario y te responderemos a la brevedad.
                 <span>
                   <br />
                   <br />
                 </span>
-                Si cuentas con un refugio que quieras sumar a nuestro apartado
-                pueden mandarnos un correo con los datos del refugio y nos
-                pondremos con contacto.
+                Si cuentas con un refugio que quisieras agregar a nuestro
+                listado, envíanos un correo con los datos del mismo y nos
+                pondremos en contacto contigo.
               </p>
             </article>
           </div>
-          <div className={styles["contacto__container"]}>
+          <div className={styles['contacto__container']}>
             <div className={styles.contacto__inputs}>
               <div>
                 <label
                   style={
                     firstName.length > 0
-                      ? { color: "#ff0004b2" }
-                      : { color: "#575756" }
+                      ? { color: '#ff0004b2' }
+                      : { color: '#575756' }
                   }
                   htmlFor="nombre"
                 >
@@ -168,8 +172,8 @@ const Contacto = () => {
                 <label
                   style={
                     lastName.length > 0
-                      ? { color: "#ff0004b2" }
-                      : { color: "#575756" }
+                      ? { color: '#ff0004b2' }
+                      : { color: '#575756' }
                   }
                   htmlFor="apellido"
                 >
@@ -191,8 +195,8 @@ const Contacto = () => {
                 <label
                   style={
                     email.length > 0
-                      ? { color: "#ff0004b2" }
-                      : { color: "#575756" }
+                      ? { color: '#ff0004b2' }
+                      : { color: '#575756' }
                   }
                   htmlFor="email"
                 >
@@ -214,8 +218,8 @@ const Contacto = () => {
                 <label
                   style={
                     phone.length > 0
-                      ? { color: "#ff0004b2" }
-                      : { color: "#575756" }
+                      ? { color: '#ff0004b2' }
+                      : { color: '#575756' }
                   }
                   htmlFor="telefono"
                 >
@@ -237,8 +241,8 @@ const Contacto = () => {
                 <label
                   style={
                     message.length > 0
-                      ? { color: "#ff0004b2" }
-                      : { color: "#575756" }
+                      ? { color: '#ff0004b2' }
+                      : { color: '#575756' }
                   }
                   htmlFor="mensaje"
                 >
@@ -259,7 +263,7 @@ const Contacto = () => {
                 />
               </div>
             </div>
-            <div className={styles["contact__send"]}>
+            <div className={styles['contact__send']}>
               <BtnPrincipal texto="Enviar" />
             </div>
           </div>
